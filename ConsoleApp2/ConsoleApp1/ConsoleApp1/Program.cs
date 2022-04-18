16be500908d0f631b33408d0f3338902d699230f
austr <austr@DESKTOP-UL4DLJQ>
2022-04-18 21:11:31 +0300

    Majas darbs 2

--- ConsoleApp2/ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,178 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World, it's a nice day!");
 //comment
+/*
+string x = "4";
+Console.WriteLine("Hey, izvadi kaut ko lūdzu, piemēram, x= " + x);
+
+int vecums = 55;
+double money = 55.22;
+Console.WriteLine("My age is not : " + vecums + " weel, not yet :)" + "some money: " + money);
+
+char simbols = 'B';
+Console.WriteLine(simbols);
+
+bool iscat = false;
+
+if(true == iscat)
+{
+    iscat = true;
+    Console.WriteLine("iekš ifija: " + iscat);
+}
+else
+{
+    iscat = false;
+    Console.WriteLine("iekš elses: " + iscat);
+}
+
+
+Console.WriteLine("---===NEW SECTION===---");
+
+int a = 5;
+int b = 2;
+
+int sum = a + b;
+Console.WriteLine("Summa : " + sum);
+
+int diff = b - a;
+Console.WriteLine("Starpība  : " + diff);
+
+int multipl = a * b;
+Console.WriteLine("Reizina  : " + multipl);
+
+double divide =(double) a / b; //kastošana
+Console.WriteLine("Dala : " + divide); //atgriež tikai veselu skaitli 
+
+int divisionLeft = a % b;
+Console.WriteLine("Dalijum atlikums  : " + divisionLeft);
+
+Console.WriteLine("---===NEW SECTION===---");
+
+a++;
+b--;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+int num1 = 10;
+int num2 = 10;
+
+bool isEqual = (num1 == num2);
+bool isNotEqual = (num1 != num2);
+bool isLarger = (num1 > num2);
+bool isSmaller = (num1 < num2);
+bool isLargerEqual = (num1 >= num2);
+bool isSmallerEqual = (num1 <= num2);
+
+Console.WriteLine(isEqual);
+Console.WriteLine(isNotEqual);
+Console.WriteLine(isLarger);
+Console.WriteLine(isSmaller);
+Console.WriteLine(isLargerEqual);
+Console.WriteLine(isSmallerEqual);
+
+
+Console.WriteLine("---===NEW SECTION===---");
+
+int d = 4;
+
+d += 2; // x = x + 2
+Console.WriteLine(d);
+
+d -= 3; // x = x - 2
+Console.WriteLine(d);
+
+
+Console.WriteLine("---===NEW SECTION===---");
+
+
+
+//1.Prasa ievadīt lietotajvardu 
+
+Console.Write("Ludzu ievadiet savu vārdu: ");
+
+//2. Sagaidam lietotaja vardu
+
+string userName = Console.ReadLine();
+
+
+//3. Izvadam lietotajvardu
+
+Console.WriteLine("Lietotāja vārds: " + userName);
+  
+
+Console.Write("Ludzu ievadiet skaitli: ");
+string userNumberStr = Console.ReadLine();
+int userNumber = int.Parse(userNumberStr);
+
+int bytenLarger = userNumber + 10;
+Console.WriteLine("Lielaks par 10: " + bytenLarger);
+
+
+
+int positiveNumber = 15;
+int negativeNumber = -15;
+
+int positiveAbs = Math.Abs(positiveNumber);
+int negativeAbs = Math.Abs(negativeNumber);
+
+Console.WriteLine("Default negative: " + negativeNumber);
+Console.WriteLine(positiveAbs);
+Console.WriteLine(negativeAbs);
+
+
+Console.WriteLine();
+Console.WriteLine("==================================");
+Console.WriteLine();
+
+
+int maxNumberHigh = 40;
+int maxNumberLow = 29;
+
+int largerNumber = Math.Max(maxNumberHigh, maxNumberLow);
+
+Console.WriteLine("Larger number: " + largerNumber);
+
+
+Console.WriteLine();
+Console.WriteLine("==================================");
+Console.WriteLine();
+
+
+int minNumberHigh = 40;
+int minNumberLow = 29;
+
+int smallerNumber = Math.Min(minNumberHigh, minNumberLow);
+
+Console.WriteLine("Smallest number: " + smallerNumber);
+
+Console.WriteLine();
+Console.WriteLine("==================================");
+Console.WriteLine();
+
+double roundableNumber = 145.58789;
+double rounded = Math.Round(roundableNumber, 2);
+
+Console.WriteLine("Number rounded: " + rounded);
+
+*/
+
+Console.WriteLine();
+Console.WriteLine("==================================");
+Console.WriteLine();
+
+string name = "Artis";
+int age2 = 80;
+
+string formattedMessage = $"Hello {name} it is very nice to see you at your age {age2} wich ios very gracefull!";
+
+string formattedMessage2 = "Hello " + name + " it is very nice to see you at your age " + age2 + " wich ios very gracefull!";
+
+Console.WriteLine(formattedMessage);
+Console.WriteLine(formattedMessage2);
+
+
+
+Console.WriteLine();
+Console.WriteLine("==================================");
+Console.WriteLine();
---
+++ md2/majasDarbs2/majasDarbs2/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("----==== 1. uzdevums ====----");
+
+Console.WriteLine("Kā tevi sauc?");
+string userName = Console.ReadLine();
+Console.WriteLine("Sveiks, " + userName  + "!");
+
+
+Console.WriteLine("----==== 2. uzdevums ====----");
+
+Console.WriteLine("Kāds ir tavs vecums?");
+int userAge = int.Parse(Console.ReadLine());
+int addNumberToUserAge = userAge + 1;
+Console.WriteLine("Nākamgad tev paliks " + addNumberToUserAge + ", Tu esi pilngadīgs!");
+
+Console.WriteLine("----==== 3. uzdevums / 4. udevumus ====----");
+
+Console.WriteLine("Lūdzu ievadiet pirmo skaitli: ");
+int firstNr = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu ievadiet otro skaitli: ");
+int secondNr = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu ievadiet trešo skaitli: ");
+int thirdNr = int.Parse(Console.ReadLine());
+
+int getLarger1 = Math.Max(firstNr, secondNr);
+int getLarger2 = Math.Max(getLarger1, thirdNr);
+Console.WriteLine("Lielākais ievadītais lietotāja skaitlis: " + getLarger2);
+
+
+int getSmaller1 = Math.Min(firstNr, secondNr);
+int getSmaller2 = Math.Min(getSmaller1, thirdNr);
+Console.WriteLine("Mazākais ievadītais lietotāja skaitlis: " + getSmaller2);
+
+Console.WriteLine("----==== 5. uzdevums ====----");
+
+Console.WriteLine("Lūdzu ievadiet pirmo skaitli: ");
+int divide1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu ievadiet otro skaitli: ");
+int divide2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dalījuma atlikums dalot " + divide1 + " ar " + divide2 + " ir: " + divide1 % divide2);
+
+Console.WriteLine("----==== 6. uzdevums ====----");
+
+bool evenNumber = true;
+Console.WriteLine("Lūdzu ievadiet skaitli: ");
+int userNumber = int.Parse(Console.ReadLine());
+evenNumber = userNumber % 2 == 0;
+Console.WriteLine("Vai ievadītais skaitlis ir pāra skaitlis:  " + evenNumber);
+
+Console.WriteLine("----==== 7. uzdevums ====----");
+
+Console.WriteLine("Lūdzu ievadiet taisnstūra garākās malas garumu: ");
+decimal longSide = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu ievadiet taisnstūra īsākās malas garumu: ");
+decimal smallSide = decimal.Parse(Console.ReadLine());
+
+decimal result = longSide * smallSide;
+Console.WriteLine("Taisnstūra laukums ir : " + Math.Round(result, 2));
+
+Console.WriteLine("----==== 8. uzdevums ====----");
+
+Console.WriteLine("Lūdzu ievadiet trijstūra malas garumu: ");
+int triangelSide = int.Parse(Console.ReadLine());
+//S= (a*b)/2
+
+int result2 = (triangelSide * triangelSide) / 2;
+Console.WriteLine("Vienādsānu taisnleņķa trijstūra laukums ir : " + result2);
+
+Console.WriteLine("----==== 9. uzdevums ====----");
+
+Console.WriteLine("Kāds ir Tavs vārds?");
+string name = Console.ReadLine();
+Console.WriteLine("Kāds ir tavs vecums?");
+int age  = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sveiks/a, {name}, tavs vecums ir {age}!");
